65c596f0b94ec2bb5763d8c474b28c31c8ab22a9
Michael Strasser <mstrass@thoughtworks.com>
2020-07-11 01:38:44 +1000

    Non-working example with oscillating list index

--- src/main/kotlin/mjs/homeschooling/HomeSchooling.kt
+++ src/main/kotlin/mjs/homeschooling/HomeSchooling.kt
@@ -6,21 +6,39 @@ data class Assignment(
         val childTwoTasks: List<Task> = emptyList(),
         val childThreeTasks: List<Task> = emptyList()
 )
-val noAssignment = Assignment(false)
 
-fun divideBetweenThreeChildren(allTasks: List<Task>) = when {
-        allTasks.size < 3 -> noAssignment
-        sumPoints(allTasks) % 3 != 0 -> noAssignment
-        else -> splitTasks(allTasks)
-    }
+val NO_ASSIGNMENT = Assignment(false)
 
-fun splitTasks(allTasks: List<Task>): Assignment {
-    val childOneTasks = mutableListOf<Task>()
-    val childTwoTasks = mutableListOf<Task>()
-    val childThreeTasks = mutableListOf<Task>()
-    childOneTasks.add(allTasks[0])
-    childTwoTasks.add(allTasks[1])
-    childThreeTasks.add(allTasks[2])
-    return Assignment(true, childOneTasks, childTwoTasks, childThreeTasks)
+fun divideTasks(allTasks: List<Task>): Assignment {
+    if (allTasks.size < 3) return NO_ASSIGNMENT
+
+    val allPoints = sumPoints(allTasks)
+    if (allPoints % 3 != 0) return NO_ASSIGNMENT
+
+    val pointsPerChild = allPoints / 3
+    val allTasksDesc = allTasks.sortedByDescending(Task::points)
+    if (allTasksDesc.first().points > pointsPerChild) return NO_ASSIGNMENT
+
+    return splitTasks(allTasksDesc, pointsPerChild)
 }
 
+fun splitTasks(allTasksDesc: List<Task>, pointsPerChild: Int): Assignment {
+    val childTasks: List<MutableList<Task>> = listOf(mutableListOf(), mutableListOf(), mutableListOf())
+
+    allTasksDesc.zip(oscillatingIndexer(allTasksDesc.size)).forEach { (task, idx) ->
+        childTasks[idx].add(task)
+        if (sumPoints(childTasks[idx]) > pointsPerChild) return NO_ASSIGNMENT
+    }
+
+    return Assignment(true, childTasks[0], childTasks[1], childTasks[2])
+}
+
+fun oscillatingIndexer(count: Int): Iterable<Int> {
+    val base = listOf(
+            0, 1, 2, 2, 1, 0, 0, 1, 2, 2, 1, 0,
+            0, 1, 2, 2, 1, 0, 0, 1, 2, 2, 1, 0,
+            0, 1, 2, 2, 1, 0, 0, 1, 2, 2, 1, 0,
+            0, 1, 2, 2, 1, 0, 0, 1, 2, 2, 1, 0
+    )
+    return base.subList(0, count)
+}
--- src/test/kotlin/mjs/homeschooling/HomeSchoolingSpek.kt
+++ src/test/kotlin/mjs/homeschooling/HomeSchoolingSpek.kt
@@ -9,31 +9,28 @@ import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.random.Random
 
+fun tasks(vararg pts: Int) = pts.map { Task("X", it) }
 
 internal object HomeSchoolingSpek : Spek({
     describe("divide tasks between three children") {
+
         it("fewer than three tasks cannot be assigned") {
-            (1..2).forEach { numTasks ->
-                divideBetweenThreeChildren(randomTasks(numTasks)).apply {
-                    assertThat(canBeSplit).isFalse()
-                }
-            }
+            assertThat(divideTasks(tasks(3)).canBeSplit).isFalse()
+            assertThat(divideTasks(tasks(5, 4)).canBeSplit).isFalse()
         }
-        describe("three tasks") {
-            it("with the same points each can be assigned") {
-                val pointsPerTask = Random.nextInt(1, 10)
-                divideBetweenThreeChildren(listOf(
-                        Task("A", pointsPerTask),
-                        Task("B", pointsPerTask),
-                        Task("C", pointsPerTask)
-                )).apply {
-                    assertThat(canBeSplit).isTrue()
-                    listOf(childOneTasks, childTwoTasks, childThreeTasks).forEach { tasks ->
-                        assertThat(tasks).hasSize(1)
-                        assertThat(tasks.first().points).isEqualTo(pointsPerTask)
-                    }
-                }
-            }
+
+        it("three tasks with the same points each can be assigned") {
+            val points = Random.nextInt(1, 10)
+            assertThat(divideTasks(tasks(points, points, points)).canBeSplit).isTrue()
+        }
+
+        it("when largest task is more than one third they cannot be assigned") {
+            assertThat(divideTasks(tasks(8, 10, 5, 2, 4)).canBeSplit).isFalse()
+            assertThat(divideTasks(tasks(5, 4, 1, 2, 7, 2, 3, 13)).canBeSplit).isFalse()
+        }
+
+        it("works for Karen’s example") {
+            assertThat(divideTasks(tasks(5, 4, 1, 2, 7, 8, 3)).canBeSplit).isTrue()
         }
     }
 })
