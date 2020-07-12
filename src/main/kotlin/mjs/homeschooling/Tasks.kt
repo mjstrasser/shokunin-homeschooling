581812652c5f6a1f90be0a28285c001a30913783
Michael Strasser <mstrass@thoughtworks.com>
2020-07-12 11:48:18 +1000

    Task name generation with multiple-letter names

--- src/main/kotlin/mjs/homeschooling/Tasks.kt
+++ src/main/kotlin/mjs/homeschooling/Tasks.kt
@@ -8,8 +8,12 @@ typealias TaskList = List<Task>
 
 fun TaskList.points() = map(Task::points).sum()
 
-fun randomTasks(numTasks: Int, maxPoints: Int = 10): TaskList = (0 until numTasks)
-        .map { Task(('A' + it).toString(), Random.nextInt(maxPoints) + 1) }
+tailrec fun nameFor(num: Int, name: String = ""): String =
+        if (num == 0) name
+        else nameFor((num - 1) / 26, ('A' + (num - 1) % 26).toString() + name)
+
+fun randomTasks(numTasks: Int, maxPoints: Int = 10): TaskList =
+        (1..numTasks).map { Task(nameFor(it), Random.nextInt(maxPoints) + 1) }
 
 data class ChildTasks(val name: String, val tasks: TaskList = emptyList())
 
--- src/test/kotlin/mjs/homeschooling/TasksSpek.kt
+++ src/test/kotlin/mjs/homeschooling/TasksSpek.kt
@@ -7,15 +7,38 @@ import org.spekframework.spek2.style.specification.describe
 import kotlin.random.Random
 
 internal object TasksSpek : Spek({
+    describe("nameFor") {
+        it("returns tasks names with single letters up to 26") {
+            assertThat(nameFor(1)).isEqualTo("A")
+            assertThat(nameFor(17)).isEqualTo("Q")
+            assertThat(nameFor(26)).isEqualTo("Z")
+        }
+        it("returns task names with multiple letters for more than 26") {
+            assertThat(nameFor(27)).isEqualTo("AA")
+            assertThat(nameFor(28)).isEqualTo("AB")
+            assertThat(nameFor(26 + 26)).isEqualTo("AZ")
+            assertThat(nameFor(26 + 26 * 26)).isEqualTo("ZZ")
+        }
+    }
+
     describe("randomTasks") {
         it("returns a list of tasks") {
-            val nTasks = Random.nextInt(26)
+            val nTasks = Random.nextInt(1000)
             assertThat(randomTasks(nTasks).size).isEqualTo(nTasks)
         }
-        it("returns tasks from A to the nth letter") {
-            assertThat(randomTasks(3).last().name).isEqualTo("C")
-            assertThat(randomTasks(11).last().name).isEqualTo("K")
-            assertThat(randomTasks(23).last().name).isEqualTo("W")
+        it("returns task names from A to the nth letter up to Z") {
+            randomTasks(3).apply {
+                assertThat(first().name).isEqualTo("A")
+                assertThat(last().name).isEqualTo("C")
+            }
+            randomTasks(11).apply {
+                assertThat(first().name).isEqualTo("A")
+                assertThat(last().name).isEqualTo("K")
+            }
+            randomTasks(26).apply {
+                assertThat(first().name).isEqualTo("A")
+                assertThat(last().name).isEqualTo("Z")
+            }
         }
     }
 })
