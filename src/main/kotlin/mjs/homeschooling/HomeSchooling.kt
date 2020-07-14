75adbe791542daa05318cf805333cfc2c92f5c0b
Michael Strasser <mstrass@thoughtworks.com>
2020-07-14 17:59:21 +1000

    Clikt command parsing

--- src/main/kotlin/mjs/homeschooling/HomeSchooling.kt
+++ src/main/kotlin/mjs/homeschooling/HomeSchooling.kt
@@ -1,33 +1,29 @@
 package mjs.homeschooling
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.multiple
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.int
 
 
 class HomeSchooling : CliktCommand() {
-    val tasks: String? by option("-t", "--tasks",
+    private val tasks by argument(name="tasks",
             help = "List of tasks to allocate for homeschooling"
-    )
-    val randomTasks: Int? by option("-r", "--random-tasks",
+    ).multiple()
+    private val randomTasks: Int? by option("-r", "--random-tasks",
             help="Randomly create a set of tasks to allocate for homeschooling"
     ).int()
 
     override fun run() {
-        echo("Hello")
+        val tasksToAllocate = selectTasks()
+        echo(tasksToAllocate)
     }
+
+    private fun selectTasks() =
+            randomTasks
+                    ?.let { randomTasks(it) }
+                    ?: tasks.map(::parseTask)
 }
 
 fun main(args: Array<String>) = HomeSchooling().main(args)
-
-fun processTasks(randomTasks: Int?, tasks: String?) {
-    val runTasks = randomTasks?.let {
-        randomTasks(it)
-    } ?: tasks?.let {
-        parseTasks(it)
-    }
-    print(runTasks)
-}
-
-fun parseTasks(tasks: String): TaskList = tasks.split(",")
-        .map { Task(it.take(1), it.drop(1).toInt()) }
